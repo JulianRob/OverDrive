d88584df7d3ffc59e30b957c24e2bc8c136becea
Julian Ventura <julian.ventura20@gmail.com>
2018-03-02 20:07:40 -0500

    Inital Commit

---
+++ Game OverDrive/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector3 mouse = Input.mousePosition;
+		mouse.z = 10;
+		transform.position = Camera.main.ScreenToWorldPoint(mouse);	
+	}
+}
